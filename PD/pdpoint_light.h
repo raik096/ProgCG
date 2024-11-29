215d9d526c3f7076c75101bf681fd6d2dfa6567d
&&re <153316758+raik096@users.noreply.github.com>
2024-11-29 01:42:30 +0100

    Create pdpoint_light.h

---
+++ PD/pdpoint_light.h
@@ -0,0 +1,63 @@
+#ifndef MATERIAL_pointL_H
+#define MATERIAL_pointL_H
+#include <glm/glm.hpp>  
+#include <glm/ext.hpp>
+
+#include <vector>
+
+#include "texture.h"
+
+
+struct point_light
+{
+    glm::vec3 lightPos;
+    glm::mat4 light_matrix;
+    texture cubemapDepth;
+
+    void set(glm::vec3 position)
+    {
+        lightPos = position;
+        float aspect = (float)sm_size_x/(float)sm_size_y;
+        float near = 1.0f;
+        float far = 25.0f;
+        glm::mat4 proj = glm::perspective(glm::radians(120.0f), aspect, near, far);
+
+        // Direzione verso il basso
+        glm::vec3 direction = glm::vec3(0.0f, -1.0f, 0.0f);
+        glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f); 
+
+        glm::mat4 view = glm::lookAt(position, position + direction, up);
+
+        light_matrix = proj * view;
+    }
+
+
+	// size of one face in texels
+	int sm_size_x, sm_size_y;
+};
+
+// ogni istanza di head_light corrisponde ad un faro
+struct headl_light
+{
+    glm::mat4 light_matrix;
+    int sm_size_x, sm_size_y;
+
+    void set(glm::mat4 view)
+    {
+
+        float aspect = (float)sm_size_x/(float)sm_size_y;
+        float near = 0.1f;
+        float far = 25.0f;
+        //float distance_light = 0.5;
+        //glm::mat4 proj = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 0.1f * 2.0f);
+        
+        glm::mat4 proj = glm::perspective(glm::radians(120.0f), aspect, near, far);
+        //glm::mat4 proj_matrix = glm::perspective(3.14f/2.f,1.0f,0.1f, distance_light*2.f);
+        light_matrix = proj * view;
+    
+    }
+    
+};
+
+
+#endif
